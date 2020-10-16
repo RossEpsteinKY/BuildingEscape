3a44721ffb539ccb179040def76f184182c8dcb3
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2020-10-16 10:30:27 -0400

    Added raycast to player bp

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,5 +1,6 @@
 // Copyright Mad Skald Studios
 
+#include "DrawDebugHelpers.h"
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 #include "Grabber.h"
@@ -43,9 +44,24 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		OUT PlayerViewpointRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("VIEW LOC: %s VIEW ROT: %s"),
+	/*UE_LOG(LogTemp, Warning, TEXT("VIEW LOC: %s VIEW ROT: %s"),
 		*PlayerViewpointLocation.ToString(),
 		*PlayerViewpointRotation.ToString()
+	);*/
+
+	//Draw line from player indicating reach
+
+	FVector LineTraceEnd = PlayerViewpointLocation + PlayerViewpointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewpointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
 	);
 
 	//raycast out arm distance (reach)
